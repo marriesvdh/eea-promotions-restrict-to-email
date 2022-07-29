5960a30202c2af5563eb7237a7d4b7c579e0102b
Marries van de Hoef <git@marries.nl>
2022-07-29 17:54:59 +0200

    updated text domain

--- templates/promotion_restrict_to_email_form.template.php
+++ templates/promotion_restrict_to_email_form.template.php
@@ -21,11 +21,11 @@
 <table class="form-table" id="promotion-details-form-restrict-to-email">
     <tr>
         <th scope="row">
-            <label for="PRX_emailrestrict"><?php esc_html_e('Restrict to email address(es)', 'event-espresso-promotions-restrict-to-email'); ?></label>
+            <label for="PRX_emailrestrict"><?php esc_html_e('Restrict to email address(es)', 'eea-promotions-restrict-to-email'); ?></label>
         </th>
         <td class="field-column">
             <input type="text" class="regular-text" id="PRX_emailrestrict" name="PRX_emailrestrict" value="<?php echo $restrict_to_email_addresses; ?>">
-            <p class="description"><?php esc_html_e('This promotion can only be used when any of the attendees use one of these e-mail addresses. Separate multiple e-mail addresses with commas.', 'event-espresso-promotions-restrict-to-email'); ?></p>
+            <p class="description"><?php esc_html_e('This promotion can only be used when any of the attendees use one of these e-mail addresses. Separate multiple e-mail addresses with commas.', 'eea-promotions-restrict-to-email'); ?></p>
         </td>
     </tr>
 </table>
