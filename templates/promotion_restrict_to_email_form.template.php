e3337f5d37250221471deccf269e75a400e18a4d
Marries van de Hoef <git@marries.nl>
2022-08-01 12:07:16 +0200

    added late escape

--- templates/promotion_restrict_to_email_form.template.php
+++ templates/promotion_restrict_to_email_form.template.php
@@ -24,7 +24,7 @@
             <label for="PRX_emailrestrict"><?php esc_html_e('Restrict to email address(es)', 'eea-promotions-restrict-to-email'); ?></label>
         </th>
         <td class="field-column">
-            <input type="text" class="regular-text" id="PRX_emailrestrict" name="PRX_emailrestrict" value="<?php echo $restrict_to_email_addresses; ?>">
+            <input type="text" class="regular-text" id="PRX_emailrestrict" name="PRX_emailrestrict" value="<?php echo esc_html($restrict_to_email_addresses); ?>">
             <p class="description"><?php esc_html_e('This promotion can only be used when any of the attendees use one of these e-mail addresses. Separate multiple e-mail addresses with commas.', 'eea-promotions-restrict-to-email'); ?></p>
         </td>
     </tr>
